646200d8fbf2065072bca96fb15c77d58fcb06a1
Noah Horwitz and Peter Podrid <peter@>
2019-10-28 10:27:48 -0700

    refactor javaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
-    var hairColorInput= $("input#hairColor").val();
-    var eyeColorInput = $("input#eyeColor").val();
-    var flowerColorInput = $("input#flowerColor").val();
+    // var firstNameInput = $("input#firstName").val();
+    // var lastNameInput = $("input#lastName").val();
+    // var hairColorInput= $("input#hairColor").val();
+    // var eyeColorInput = $("input#eyeColor").val();
+    // var flowerColorInput = $("input#flowerColor").val();
 
-    $(".firstName").text(firstNameInput.toUpperCase());
-    $(".lastName").text(lastNameInput);
-    $(".hairColor").text(hairColorInput);
-    $(".eyeColor").text(eyeColorInput);
-    $(".flowerColor").text(flowerColorInput);
+    var inputs = ["firstName", "lastName", "hairColor", "eyeColor", "flowerColor"];
+
+      inputs.forEach(function(input) {
+        var userInput = $("input#" + input).val();
+        $("." + input).text(userInput)
+      });
+
+    // $(".firstName").text(firstNameInput.toUpperCase());
+    // $(".lastName").text(lastNameInput);
+    // $(".hairColor").text(hairColorInput);
+    // $(".eyeColor").text(eyeColorInput);
+    // $(".flowerColor").text(flowerColorInput);
 
 
     $("#story").show();
