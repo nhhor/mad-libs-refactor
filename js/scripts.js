90c664a96871ef36ed07cf339bbb46f3e9dcce7c
Noah Horwitz and Peter Podrid <peter@>
2019-10-28 10:16:14 -0700

    initiate project with old mad-lip files.

--- js/scripts.js
+++ js/scripts.js
@@ -11,88 +11,10 @@ $(document).ready(function() {
     $(".hairColor").text(hairColorInput);
     $(".eyeColor").text(eyeColorInput);
     $(".flowerColor").text(flowerColorInput);
-// order page
-    var quantityInput= $("input#quantity").val();
-    var addressLine1Input = $("input#addressLine1").val();
-    var addressLine2Input = $("input#addressLine2").val();
-
-    $(".quantity").text(quantityInput);
-    $(".addressLine1").text(addressLine1Input);
-    $(".addressLine2").text(addressLine2Input);
-
-    $("#story").show();
-    event.preventDefault();
-  });
-
-
-  $("#blanks2 form").submit(function(event) {
-    var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
-    var hairColorInput= $("input#hairColor").val();
-    var eyeColorInput = $("input#eyeColor").val();
-    var flowerColorInput = $("input#flowerColor").val();
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
-    $(".hairColor").text(hairColorInput);
-    $(".eyeColor").text(eyeColorInput);
-    $(".flowerColor").text(flowerColorInput);
-// order page
-    var quantityInput= $("input#quantity").val();
-    var addressLine1Input = $("input#addressLine1").val();
-    var addressLine2Input = $("input#addressLine2").val();
-
-    $(".quantity").text(quantityInput);
-    $(".addressLine1").text(addressLine1Input);
-    $(".addressLine2").text(addressLine2Input);
-
-    $("#story").show();
-    event.preventDefault();
-  });
-
-
-  $("#blanks3 form").submit(function(event) {
-    var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
-    var appointmentTypeInput = $("input#appointmentType").val();
-    var appointmentTimeInput= $("input#appointmentTime").val();
-    var appointmentTime2Input = $("input#appointmentTime2").val();
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
-    $(".appointmentType").text(appointmentTypeInput);
-    $(".appointmentTime").text(appointmentTimeInput);
-    $(".appointmentTime2").text(appointmentTime2Input);
-
-
-    $("#submit").popupForm();
-    // onclick = window.open(test.html, '', 'scrollbars=no,menubar=no,height=600,width=800,resizable=yes,toolbar=no,status=no');
-    event.preventDefault();
-  });
-// survey
-
-
-
-  $("#blanks4 form").submit(function(event) {
-    var firstNameInput = $("input#firstName").val();
-    var lastNameInput = $("input#lastName").val();
-    var rockInput = $("input#rock").val();
-    var rapInput= $("input#rap").val();
-
-
-    $(".firstName").text(firstNameInput);
-    $(".lastName").text(lastNameInput);
-    $(".rock").text(rockInput);
-    $(".rap").text(rapInput);
-
-
-
-
 
 
     $("#story").show();
     event.preventDefault();
   });
-
 
 });
